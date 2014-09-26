94e24dacca06bd36eca1850cfecf56b6f1106b6f
Raffael Schmid <raffael@yux.ch>
2014-09-26 23:34:46 +0200

    use config stuff

--- lib/amqp_helpers/publisher.rb
+++ lib/amqp_helpers/publisher.rb
@@ -7,25 +7,22 @@ module AMQPHelpers
       @config = config
     end
 
-    def publish(message, routing_key)
+    def publish(message, exchange_name, routing_key)
       Bunny.run(@config) do |connection|
-        exchange = connection.exchange(@config[:exchange], type: :topic, durable: Cony.config.durable)
+        exchange_config = @config[:exchanges][exchange_name]
+        return unless exchange_config
+
+        exchange = connection.exchange(exchange_name, exchange_config[:params])
         exchange.publish(message.to_json,
                          key: routing_key,
                          mandatory: false,
                          immediate: false,
-                         persistent: Cony.config.durable,
+                         persistent: exchange_config[:params][:durable],
                          content_type: 'application/json')
       end
     rescue => error
       Airbrake.notify_or_ignore(error) if defined?(Airbrake)
       Rails.logger.error("#{error.class}: #{error}") if defined?(Rails)
-    end
-
-    private
-
-    def amqp_url
-      "amqp://guest:guest@localhost:5672"
     end
   end
 end
