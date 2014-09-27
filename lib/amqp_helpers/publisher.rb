17d1f63682090d75229d3db56ddeaa37d05b3a79
Raffael Schmid <raffael@yux.ch>
2014-09-27 13:10:08 +0200

    add tests

--- lib/amqp_helpers/publisher.rb
+++ lib/amqp_helpers/publisher.rb
@@ -11,7 +11,7 @@ module AMQPHelpers
     def publish(message, exchange_name, routing_key)
       Bunny.run(@config) do |connection|
         exchange_config = @config[:exchanges][exchange_name]
-        return unless exchange_config
+        return false unless exchange_config
 
         exchange = connection.exchange(exchange_name, exchange_config[:params])
         exchange.publish(message.to_json,
@@ -19,6 +19,7 @@ module AMQPHelpers
                          persistent: exchange_config[:params][:durable],
                          content_type: 'application/json')
       end
+      true
     rescue => error
       Airbrake.notify_or_ignore(error) if defined?(Airbrake)
       Rails.logger.error("#{error.class}: #{error}") if defined?(Rails)
---
+++ spec/publisher_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+require 'amqp_helpers/publisher'
+
+describe AMQPHelpers::Publisher do
+  let(:config) do
+    {
+      name: 'lala',
+      queue_name: 'example-queue',
+      queue_params: { durable: false },
+      logger: Logger.new('/dev/null'),
+      connection_params: { host: 'localhost', port: 1337 },
+      exchanges: {
+        'test-topic' => {
+          params: { type: :topic, durable: false },
+          bindings: [ { routing_key: 'lala.#' } ]
+        }
+      }
+    }
+  end
+
+  let(:instance) { described_class.new(config) }
+  let(:bunny_double) { double(Bunny::Session) }
+  let(:exchange_double) { double(Bunny::Exchange) }
+
+  before do
+    allow(Bunny).to receive(:run).and_yield(bunny_double)
+    allow(bunny_double).to receive(:exchange).and_return(exchange_double)
+  end
+
+  describe '#publish' do
+    context 'exchange specified' do
+      it 'publishes the message' do
+        expect(exchange_double).to receive(:publish).
+          with('"we are testing"', key: 'lala.will.exist', persistent: false, content_type: 'application/json')
+
+        expect(instance.publish('we are testing', 'test-topic', 'lala.will.exist')).to eq(true)
+      end
+    end
+
+    context 'wrong exchange specified' do
+      it 'does not publish the message' do
+        expect(exchange_double).to_not receive(:publish)
+        expect(instance.publish('we are testing', 'undefined-topic', 'lala.will.exist')).to eq(false)
+      end
+    end
+  end
+end
