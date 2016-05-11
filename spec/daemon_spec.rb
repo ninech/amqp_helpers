8d102b56380ae3719cd3afa5c4841f9e75d2b97c
David Dieulivol <dadie@nine.ch>
2016-05-11 13:38:33 +0200

    [Rspec] Moves the specs to an RSpec 3 syntax.

--- spec/daemon_spec.rb
+++ spec/daemon_spec.rb
@@ -7,7 +7,7 @@ describe AMQPHelpers::Daemon do
     subject { described_class.new }
 
     it 'starts the AMQP loop with the given config' do
-      AMQP.should_receive(:start).with instance_of(Hash)
+      expect(AMQP).to receive(:start).with instance_of(Hash)
       subject.start
     end
 
@@ -34,52 +34,52 @@ describe AMQPHelpers::Daemon do
     subject { described_class.new(config) }
 
     before(:each) do
-      AMQP.stub(:start) do |&block|
+      allow(AMQP).to receive(:start) do |&block|
         @block = block
       end
       subject.start { |a, b| nil }
     end
 
     before(:each) do
-      EM.stub(:reactor_running?).and_return(true)
-      connection.stub(:auto_recovering?).and_return(true)
-      connection.stub(:open?).and_return(true)
-      connection.stub(:channel_max).and_return(0)
-      connection.stub(:on_connection)
-      connection.stub(:on_open)
-      connection.stub(:on_recovery)
-      connection.stub(:on_error)
-      connection.stub(:on_tcp_connection_loss)
-      connection.stub(:next_channel_id).and_return(0)
-      AMQP::Channel.any_instance.stub(:topic).and_return(exchange)
-      AMQP::Channel.any_instance.stub(:queue).and_return(queue)
-      queue.stub(:bind)
-      queue.stub(:subscribe)
+      allow(EM).to receive(:reactor_running?).and_return(true)
+      allow(connection).to receive(:auto_recovering?).and_return(true)
+      allow(connection).to receive(:open?).and_return(true)
+      allow(connection).to receive(:channel_max).and_return(0)
+      allow(connection).to receive(:on_connection)
+      allow(connection).to receive(:on_open)
+      allow(connection).to receive(:on_recovery)
+      allow(connection).to receive(:on_error)
+      allow(connection).to receive(:on_tcp_connection_loss)
+      allow(connection).to receive(:next_channel_id).and_return(0)
+      allow_any_instance_of(AMQP::Channel).to receive(:topic).and_return(exchange)
+      allow_any_instance_of(AMQP::Channel).to receive(:queue).and_return(queue)
+      allow(queue).to receive(:bind)
+      allow(queue).to receive(:subscribe)
     end
 
     it 'creates a non-durable queue' do
-      AMQP::Channel.any_instance.should_receive(:queue).with('example-queue', durable: false).and_return(queue)
+      expect_any_instance_of(AMQP::Channel).to receive(:queue).with('example-queue', durable: false).and_return(queue)
       block.call(connection)
     end
 
     it 'creates the topic' do
-      AMQP::Channel.any_instance.should_receive(:topic).with('test-topic', type: :topic, durable: false).and_return(exchange)
+      expect_any_instance_of(AMQP::Channel).to receive(:topic).with('test-topic', type: :topic, durable: false).and_return(exchange)
       block.call(connection)
     end
 
     it 'binds the queue to the exchange' do
-      queue.should_receive(:bind).with(exchange, routing_key: 'lala.#')
+      expect(queue).to receive(:bind).with(exchange, routing_key: 'lala.#')
       block.call(connection)
     end
 
     it 'subscribes' do
-      queue.should_receive(:subscribe)
+      expect(queue).to receive(:subscribe)
       block.call(connection)
     end
 
     it 'enables you to use options (e.g. ack: true) for the subscribing process' do
       subject.start(ack: true)
-      queue.should_receive(:subscribe).with(ack: true)
+      expect(queue).to receive(:subscribe).with(ack: true)
       block.call(connection)
     end
   end
